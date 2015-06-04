3a7584ef051ecc32c951832fac8f485b32b613dd
Christopher R. Palmer <crpalmer@gmail.com>
2015-06-04 15:45:55 -0400

    msm8916-common: Unlock the GPS
    Change-Id: If9fd67483fedee915d46f7b9d2a7e0851400de3c
    Signed-off-by: Ramsudharsan <ramsudharsanm@gmail.com>

--- gps/loc_api/libloc_api_50001/loc_eng.cpp
+++ gps/loc_api/libloc_api_50001/loc_eng.cpp
@@ -1412,6 +1412,7 @@ struct LocEngInit : public LocMsg {
         loc_eng_reinit(*mLocEng);
         // set the capabilities
         mLocEng->adapter->sendMsg(new LocEngSetCapabilities(mLocEng));
+        mLocEng->adapter->setGpsLock(1);
     }
     inline void locallog() const
     {
