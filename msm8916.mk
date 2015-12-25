139cfae3450a8e94c648ddbe4bb82e9e59993cbd
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-12-25 00:41:04 -0500

    msm8916-common: make media_codecs_performance device specific
    Lux has more powerful hardware than osprey and surnia.
    
    Change-Id: Iaba8699bcd4603510d6f87fecebd8f5e6b2636fc

--- msm8916.mk
+++ msm8916.mk
@@ -113,8 +113,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # OMX
 PRODUCT_PACKAGES += \
