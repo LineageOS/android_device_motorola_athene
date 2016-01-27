5ef2f800b77efada4ef117ba3deaba4d57821d83
HashBang <hashbang173@gmail.com>
2016-01-27 10:29:35 -0500

    add ro.telephony.ril.config for dual sim variants
    Change-Id: I2dc18dcda9b3bc32d89d45e7bf6d149a8a41125e

--- init/init_athene.cpp
+++ init/init_athene.cpp
@@ -159,6 +159,7 @@ static void num_sims(void) {
 
     if (dualsim == "true") {
         property_set("persist.radio.multisim.config", "dsds");
+        property_set("ro.telephony.ril.config", "simactivation");
     } else {
         property_set("persist.radio.multisim.config", "");
     }
