e927d512a5a6c241949136cf84b3288b87f9ce4e
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    athene: Allow vendor init to override props
    Change-Id: Ibf8aa134c7fae061ff3c71c05a505eb83fccc80d

--- init/init_athene.cpp
+++ init/init_athene.cpp
@@ -26,6 +26,8 @@
  */
 
 #include <stdlib.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 #include <stdio.h>
 
 #include "vendor_init.h"
@@ -35,6 +37,17 @@
 
 static void num_sims(void);
 static void target_ram(void);
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 void vendor_load_properties()
 {
