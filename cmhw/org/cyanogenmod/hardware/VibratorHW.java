b9d37c15acfe28e9059c5e3dad404cd0479d4745
Alberto97 <albertop2197@gmail.com>
2016-04-05 17:18:33 +0200

    msm8916-common: Update CMHW implementation of vibration control
    Change-Id: I9e0fce541c43ce675ebcd1489b8c3f5f2916151f

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2015-2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,34 +17,39 @@
 package org.cyanogenmod.hardware;
 
 import org.cyanogenmod.hardware.util.FileUtils;
-
 import java.io.File;
 
 public class VibratorHW {
-
-    private static String AMP_PATH = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
+    private static String DEFAULT_LEVEL_PATH = "/sys/class/timed_output/vibrator/vtg_level_default";
+    private static String LEVEL_PATH = "/sys/class/timed_output/vibrator/vtg_level";
+    private static String MAX_PATH = "/sys/class/timed_output/vibrator/vtg_max";
+    private static String MIN_PATH = "/sys/class/timed_output/vibrator/vtg_min";
 
     public static boolean isSupported() {
-        File file = new File(AMP_PATH);
-        return file.exists();
+        return new File(LEVEL_PATH).exists();
     }
 
     public static int getMaxIntensity()  {
-        return 31;
+        return Integer.parseInt(FileUtils.readOneLine(MAX_PATH));
     }
+
     public static int getMinIntensity()  {
-        return 12;
+        return Integer.parseInt(FileUtils.readOneLine(MIN_PATH));
     }
+
     public static int getWarningThreshold()  {
         return -1;
     }
+
     public static int getCurIntensity()  {
-        return Integer.parseInt(FileUtils.readOneLine(AMP_PATH));
+        return Integer.parseInt(FileUtils.readOneLine(LEVEL_PATH));
     }
+
     public static int getDefaultIntensity()  {
-        return 24;
+        return Integer.parseInt(FileUtils.readOneLine(DEFAULT_LEVEL_PATH));
     }
+
     public static boolean setIntensity(int intensity)  {
-        return FileUtils.writeLine(AMP_PATH, String.valueOf(intensity));
+        return FileUtils.writeLine(LEVEL_PATH, String.valueOf(intensity));
     }
 }
