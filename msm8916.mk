08dfe3f2befcfa3871db440c6b2016b7d445a136
Alberto97 <albertop2197@gmail.com>
2016-06-03 12:45:11 +0200

    msm8916-common: Allow using new Moto Camera
    Change-Id: Ie15115a54cc8f871310cc280f12513821cf37bc2

--- msm8916.mk
+++ msm8916.mk
@@ -46,6 +46,7 @@ PRODUCT_COPY_FILES += \
 # Motorola Camera permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/com.motorola.camera.xml:system/etc/permissions/com.motorola.camera.xml \
+    $(LOCAL_PATH)/configs/com.motorola.cameraone.xml:system/etc/permissions/com.motorola.cameraone.xml \
     $(LOCAL_PATH)/configs/com.motorola.motosignature.xml:system/etc/permissions/com.motorola.motosignature.xml
 
 # Screen density
