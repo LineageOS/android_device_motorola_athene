0057b16d4e64d6df6018477de7b3b74c1167a28a
Vache <contact@ache.mobi>
2016-08-15 14:47:53 +0200

    Import data-ipa-cfg-mgr

---
+++ display/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := pp_calib_data_mipi_mot_cmd_boe_1080p_546_v0.xml
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := pp_calib_data_mipi_mot_cmd_boe_1080p_546_v0.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := pp_calib_data_mipi_mot_cmd_lgd_1080p_546_v0.xml
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := pp_calib_data_mipi_mot_cmd_lgd_1080p_546_v0.xml
+include $(BUILD_PREBUILT)
+
