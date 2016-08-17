6094ce08b608cb4af900369780406bde3a984a79
Vache <contact@ache.mobi>
2016-08-17 08:44:37 +0200

    gps: import CAF sources LA.BR.1.3.3-02720-8976.0

--- gps/loc_api/Android.mk
+++ gps/loc_api/Android.mk
@@ -1,5 +1,3 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-
 LOCAL_PATH := $(call my-dir)
 
 # add RPC dirs if RPC is available
@@ -13,5 +11,3 @@ GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api_50001/
 
 #call the subfolders
 include $(addsuffix Android.mk, $(GPS_DIR_LIST))
-
-endif#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
