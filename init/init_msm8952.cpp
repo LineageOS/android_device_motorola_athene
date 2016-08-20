3ecb996664f70ab54a78d6c621a99681f9e8ef81
Vachounet <contact@vache-android.com>
2016-08-20 19:10:27 +0200

    init: Cleanup

--- init/init_msm8952.cpp
+++ init/init_msm8952.cpp
@@ -34,71 +34,6 @@
 
 #define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
 
-/* Target-Specific Dalvik Heap & HWUI Configuration */
-void target_ram() {
-    char ram[PROP_VALUE_MAX];
-    property_get("ro.boot.ram", ram);
-
-    if (ISMATCH(ram, "2GB")) {
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "192m");
-        property_set("dalvik.vm.heapsize", "384m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "512k");
-        property_set("dalvik.vm.heapmaxfree", "8m");
-
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "2048");
-    } else if (ISMATCH(ram, "3GB")) {
-        property_set("dalvik.vm.heapstartsize", "16m");
-        property_set("dalvik.vm.heapgrowthlimit", "265m");
-        property_set("dalvik.vm.heapsize", "512m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "2m");
-        property_set("dalvik.vm.heapmaxfree", "8m");
-
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "2048");
-    } else if (ISMATCH(ram, "4GB")) {
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "256m");
-        property_set("dalvik.vm.heapsize", "512m");
-        property_set("dalvik.vm.heaptargetutilization", "0.25");
-        property_set("dalvik.vm.heapminfree", "4m");
-        property_set("dalvik.vm.heapmaxfree", "16m");
-
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "2048");
-    }
-}
-
 void num_sims() {
     char dualsim[PROP_VALUE_MAX];
     property_get("ro.boot.dualsim", dualsim);
@@ -106,8 +41,7 @@ void num_sims() {
     if (ISMATCH(dualsim, "true")) {
         property_set("persist.radio.multisim.config", "dsds");
 	} else {
-        property_set("persist.radio.multisim.config", "");   
-        property_set("persist.radio.is_wps_enabled", "true");
+        property_set("persist.radio.multisim.config", "");
 	}
 }
 
@@ -131,12 +65,6 @@ void vendor_load_properties()
 
     /* Common for all models */
     property_set("ro.build.product", "athene");
-    property_set("ro.mot.build.customerid", "retail");
-    property_set("persist.radio.mot_ecc_custid", "common");
-    property_set("persist.radio.mot_ecc_enabled", "1");
-    property_set("persist.radio.force_get_pref", "1");
-    property_set("ro.telephony.default_network", "10,0");
-    target_ram();
     num_sims();
 
     if (ISMATCH(device_boot, "athene_13mp")) {
@@ -145,7 +73,7 @@ void vendor_load_properties()
         property_set("ro.build.description", "athene-user 6.0.1 MPJ24.139-23.4 4 release-keys");
         property_set("ro.build.fingerprint", "motorola/athene/athene:6.0.1/MPJ24.139-23.4/4:user/release-keys");
         property_set("ro.hw.fps", "false");
-		property_set("ro.product.model", "Moto G4");
+		property_set("ro.product.model", "Moto G4");	
     } else {
         /* Moto G4 Plus (XT164x) */
         property_set("ro.product.device", "athene_f");
@@ -154,7 +82,22 @@ void vendor_load_properties()
         property_set("ro.hw.fps", "true");
 		property_set("ro.product.model", "Moto G4 Plus");
     }
-
+	
+	if ((ISMATCH(sku, "XT1625")) || (ISMATCH(sku, "XT1644"))) {
+		property_set("persist.radio.is_wps_enabled", "true");
+		property_set("ro.radio.imei.sv", "4");
+	}
+	
+	if (ISMATCH(sku, "XT1621")) {
+		property_set("ro.radio.imei.sv", "3");
+	}
+	
+	if (ISMATCH(sku, "XT1626")) {
+		property_set("ro.radio.imei.sv", "2");
+		property_set("persist.radio.is_wps_enabled", "true");
+		property_set("persist.radio.pb.max.match", "10");
+	}
+	
     property_get("ro.product.device", device);
     strlcpy(devicename, device, sizeof(devicename));
     INFO("Found sku id: %s setting build properties for %s device\n", sku, devicename);
