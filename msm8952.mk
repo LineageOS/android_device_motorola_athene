50d91155804091929c104b2da70a585bb7e126b7
Shreps <shr3ps@gmail.com>
2016-08-31 22:26:35 +0200

    vendor cm should be optionnal

--- msm8952.mk
+++ msm8952.mk
@@ -254,4 +254,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product-if-exists, vendor/cm/config/common_full_phone.mk)
+
