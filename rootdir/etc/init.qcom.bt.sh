b326fd9906d81c46d0bf8017078b72e9e0ff0322
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-10-05 18:36:12 -0400

    Cleanup init.qcom.bt.sh (Kang msm8974)
    Change-Id: Ib897db9129f70dc328aa63dc8309a44f0c1219bb

--- rootdir/etc/init.qcom.bt.sh
+++ rootdir/etc/init.qcom.bt.sh
@@ -26,14 +26,8 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-#Read the arguments passed to the script
-config="$1"
-
-BLUETOOTH_SLEEP_PATH=/proc/bluetooth/sleep/proto
 LOG_TAG="qcom-bluetooth"
 LOG_NAME="${0}:"
-
-hciattach_pid=""
 
 loge ()
 {
@@ -195,71 +189,10 @@ esac
 
 }
 
-start_hciattach ()
-{
-  /system/bin/hciattach -n $BTS_DEVICE $BTS_TYPE $BTS_BAUD &
-  hciattach_pid=$!
-  logi "start_hciattach: pid = $hciattach_pid"
-  echo 1 > $BLUETOOTH_SLEEP_PATH
-}
-
-kill_hciattach ()
-{
-  echo 0 > $BLUETOOTH_SLEEP_PATH
-  logi "kill_hciattach: pid = $hciattach_pid"
-  ## careful not to kill zero or null!
-  kill -TERM $hciattach_pid
-  # this shell doesn't exit now -- wait returns for normal exit
-}
-
-logi "init.qcom.bt.sh config = $config"
-case "$config" in
-    "onboot")
-        program_bdaddr
-        exit 0
-        ;;
-    *)
-        ;;
-esac
-
-# mimic hciattach options parsing -- maybe a waste of effort
-USAGE="hciattach [-n] [-p] [-b] [-t timeout] [-s initial_speed] <tty> <type | id> [speed] [flow|noflow] [bdaddr]"
-
-while getopts "blnpt:s:" f
-do
-  case $f in
-  b | l | n | p)  opt_flags="$opt_flags -$f" ;;
-  t)      timeout=$OPTARG;;
-  s)      initial_speed=$OPTARG;;
-  \?)     echo $USAGE; exit 1;;
-  esac
-done
-shift $(($OPTIND-1))
-
-# Note that "hci_qcomm_init -e" prints expressions to set the shell variables
-# BTS_DEVICE, BTS_TYPE, BTS_BAUD, and BTS_ADDRESS.
-
-#Selectively Disable sleep
-BOARD=`getprop ro.board.platform`
-STACK=`getprop ro.qc.bluetooth.stack`
-
-# BR/EDR & LE power class configurations
 POWER_CLASS=`getprop qcom.bt.dev_power_class`
 LE_POWER_CLASS=`getprop qcom.bt.le_dev_pwr_class`
 
-#find the transport type
-TRANSPORT=`getprop ro.qualcomm.bt.hci_transport`
-logi "Transport : $TRANSPORT"
-case $STACK in
-    "bluez")
-       logi "** Bluez stack **"
-    ;;
-    *)
-       logi "** Bluedroid stack **"
-       setprop bluetooth.status off
-    ;;
-esac
-
+setprop bluetooth.status off
 
 case $POWER_CLASS in
   1) PWR_CLASS="-p 0" ;
@@ -290,51 +223,12 @@ eval $(/system/bin/hci_qcomm_init -e $PWR_CLASS $LE_PWR_CLASS && echo "exit_code
 case $exit_code_hci_qcomm_init in
   0) logi "Bluetooth QSoC firmware download succeeded, $BTS_DEVICE $BTS_TYPE $BTS_BAUD $BTS_ADDRESS";;
   *) failed "Bluetooth QSoC firmware download failed" $exit_code_hci_qcomm_init;
-     case $STACK in
-         "bluez")
-            logi "** Bluez stack **"
-         ;;
-         *)
-            logi "** Bluedroid stack **"
-            setprop bluetooth.status off
-        ;;
-     esac
+
+     setprop bluetooth.status off
 
      exit $exit_code_hci_qcomm_init;;
 esac
 
-# init does SIGTERM on ctl.stop for service
-trap "kill_hciattach" TERM INT
-
-case $TRANSPORT in
-    "smd")
-       case $STACK in
-           "bluez")
-              logi "** Bluez stack **"
-              echo 1 > /sys/module/hci_smd/parameters/hcismd_set
-           ;;
-           *)
-              logi "** Bluedroid stack **"
-              setprop bluetooth.status on
-           ;;
-       esac
-     ;;
-     *)
-        logi "start hciattach"
-        start_hciattach
-        case $STACK in
-            "bluez")
-               logi "Bluetooth is turning On with Bluez stack "
-            ;;
-            *)
-               logi "** Bluedroid stack **"
-               setprop bluetooth.status on
-            ;;
-        esac
-
-        wait $hciattach_pid
-        logi "Bluetooth stopped"
-     ;;
-esac
+setprop bluetooth.status on
 
 exit 0
