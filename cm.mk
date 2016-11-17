dbf1e79d966ca00848b7b6d4731e1343602295c6
Vachounet <vachounet@live.fr>
2016-11-17 09:27:48 +0100

    athene: set TARGET_BOOTANIMATION_HALF_RES to speed up boots
    Change-Id: I2a6409f4204f6f7890aa07dd81a8f3fadec08bdf

--- cm.mk
+++ cm.mk
@@ -20,6 +20,7 @@ $(call inherit-product, device/motorola/athene/full_athene.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
