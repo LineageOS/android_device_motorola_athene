2788ef4350f6b715fad461200ddcb3c0ccaed14d
rahulsnair <rahulsnair30@gmail.com>
2017-01-25 08:15:48 +0000

    athene: fingerprint update to NPJ25.93-14
    Change-Id: Ifcfc296e03b5ad5397a447e522d1cd1a4814db6e

--- init/init_athene.cpp
+++ init/init_athene.cpp
@@ -69,15 +69,15 @@ void vendor_load_properties()
     if (device_boot == "athene_13mp") {
         /* Moto G4 (XT162x) */
         property_set("ro.product.device", "athene");
-        property_set("ro.build.description", "athene-user 7.0 NPJ25.93-9 1 release-keys");
-        property_set("ro.build.fingerprint", "motorola/athene/athene:7.0/NPJ25.93-9/1:user/release-keys");
+        property_set("ro.build.description", "athene-user 7.0 NPJ25.93-14 16 release-keys");
+        property_set("ro.build.fingerprint", "motorola/athene/athene:7.0/NPJ25.93-14/16:user/release-keys");
         property_set("ro.product.model", "Moto G4");
         property_set("ro.telephony.default_network", "10");
     } else {
         /* Moto G4 Plus (XT164x) */
         property_set("ro.product.device", "athene_f");
-        property_set("ro.build.description", "athene_f-user 7.0 NPJ25.93-9 1 release-keys");
-        property_set("ro.build.fingerprint", "motorola/athene_f/athene_f:7.0/NPJ25.93-9/1:user/release-keys");
+        property_set("ro.build.description", "athene_f-user 7.0 NPJ25.93-14 16 release-keys");
+        property_set("ro.build.fingerprint", "motorola/athene_f/athene_f:7.0/NPJ25.93-14/16:user/release-keys");
         property_set("ro.product.model", "Moto G4 Plus");
         property_set("ro.telephony.default_network", "10,10");
     }
