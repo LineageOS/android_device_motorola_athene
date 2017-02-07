9e197e82fef1676f7b0ce782c31f164c868e8d2b
Christopher R. Palmer <crpalmer@gmail.com>
2017-02-07 06:26:48 -0500

    cmactions: Improve doze denial thresholds
    Previously we would deny a doze if it had been more than 1.5 seconds
    since the last doze started.  A doze lasts about 7 seconds so that
    was a meaningless test except as it related to the first doze after
    the screen turned off.
    
    Break that constant into two constants (time since the screen turned
    off and time since the last doze started) to allow them to be
    independently configured.
    
    Make the screen on delay longer (3 seconds) to avoid excessively
    dozing as you turn the screen off.
    
    Make the inter-doze time 10 seconds which seems to be a balance between
    waving your hand fruitlessly toward your phone and triggering too
    easily.
    
    Tihs is a hacky fix to the fact that the stow sensor seems to erroneously
    trigger a false followed short by a true if stowed when the doze finishes.
    
    Fixes Lineage: BUGBASH-42
    
    Change-Id: I881d0f7821b693cb8c5dd80268e0c807dd2f5fc9

--- cmactions/src/com/cyanogenmod/settings/device/DozePulseAction.java
+++ cmactions/src/com/cyanogenmod/settings/device/DozePulseAction.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,11 +24,13 @@ import android.util.Log;
 public class DozePulseAction implements SensorAction, ScreenStateNotifier {
     private static final String TAG = "CMActions";
 
-    private static final int DELAY_BETWEEN_DOZES_IN_MS = 1500;
+    private static final int DELAY_BEFORE_FIRST_DOZE_IN_MS = 3000;
+    private static final int DELAY_BETWEEN_DOZES_IN_MS = 10000;
 
     private final Context mContext;
 
     private long mLastDoze;
+    private long mLastScreenOff;
 
     public DozePulseAction(Context context) {
         mContext = context;
@@ -39,7 +42,7 @@ public class DozePulseAction implements SensorAction, ScreenStateNotifier {
 
     @Override
     public void screenTurnedOff() {
-        mLastDoze = System.currentTimeMillis();
+        mLastScreenOff = System.currentTimeMillis();
     }
 
     public void action() {
@@ -51,13 +54,17 @@ public class DozePulseAction implements SensorAction, ScreenStateNotifier {
 
     public synchronized boolean mayDoze() {
         long now = System.currentTimeMillis();
-        if (now - mLastDoze > DELAY_BETWEEN_DOZES_IN_MS) {
-            Log.d(TAG, "Allowing doze");
-            mLastDoze = now;
-            return true;
-        } else {
-            Log.d(TAG, "Denying doze");
+
+        if (now - mLastScreenOff < DELAY_BEFORE_FIRST_DOZE_IN_MS) {
+            Log.d(TAG, "Denying doze due to DELAY_BEFORE_FIRST_DOZE_IN_MS");
+            return false;
+        } else if (now - mLastDoze < DELAY_BETWEEN_DOZES_IN_MS) {
+            Log.d(TAG, "Denying doze due to DELAY_BETWEEN_DOZES");
             return false;
         }
+
+        Log.d(TAG, "Allowing doze");
+        mLastDoze = now;
+        return true;
     }
 }
