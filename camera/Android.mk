febf6975cceb22719ae42925b1ed47609d3d3108
Danny Baumann <dannybaumann@web.de>
2017-05-31 23:09:53 +0200

    athene: add and build camera wrapper
    Waits for sensorservice initialization before opening the actual HAL.
    
    Change-Id: I113fd7baf6602a6c22e461fa92c16fcfb95d9298

---
+++ camera/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libgui libutils
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
--- device.mk
+++ device.mk
@@ -103,6 +103,7 @@ PRODUCT_COPY_FILES +=  \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.msm8952 \
     libbson \
     Snap
 
