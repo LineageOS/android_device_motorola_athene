2fdb82c4f6cf389bd8b72229cfc1f1422b22f89f
Anoop Kumar <ayeruk@codeaurora.org>
2017-06-02 13:33:54 +0200

    athene: Add AEP support config file
    This change exposes the support for Android extension pack (AEP) in the graphics driver by copying the android.hardware.opengles.aep.xml to the /system/etc/permissions directory on the device.
    
    Change-Id: I94280091af571ef21b98b09dea60201f3d0e0066

--- device.mk
+++ device.mk
@@ -53,7 +53,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
 
 # Motorola Camera permissions
 PRODUCT_COPY_FILES += \
